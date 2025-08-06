49e9ad45a1431cd66891c7f798f9a7f829d980ef
swafwana <swafwanasheri17@gmail.com>
2025-08-06 15:13:40 +0530

    Update mainactivity.java

--- mainactivity.java
+++ mainactivity.java
@@ -1,1 +1,41 @@
+package com.example.loginform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView Username = (TextView) findViewById((R.id.Username));
+        TextView Password = (TextView) findViewById((R.id.Password));
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Username.getText().toString().equals("admin") && Password.getText().toString().equals("123456")) {
+
+                    Toast.makeText(MainActivity.this, "Login Successfully:)", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed:(", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+    }
+}
+
+
+
+
+
+
 
